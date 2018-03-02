db0c04e09e0a66201ef41b9b5dd379bca4cdb68f
zencore <dobetter@zencore.cn>
2018-03-02 21:09:47 +0800

    fix test

--- test/app.py
+++ test/app.py
@@ -3,14 +3,18 @@ import time
 import signal
 import datetime
 
+def log(msg):
+    with open("app.log", "a", encoding="utf-8") as output:
+        print(msg, file=output)
+
 def main(config):
     global stop
     stop = False
     def on_exit(sig, frame):
         global stop
         stop = True
+        log("got exit signal...")
     signal.signal(signal.SIGTERM, on_exit)
     while not stop:
-        with open("app.log", "a", encoding="utf-8") as output:
-            print(str(datetime.datetime.now()), file=output)
+        log(datetime.datetime.now())
         time.sleep(1)
